86f365e6d88f234f70af195797e03962d53cd0e5
De Biasi Riccardo <debiasi.riccardo@osdbsesto>
2020-01-24 10:00:48 +0100

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.getElementByIdById("h1id").innerHTML = "ciao come stai?"; 
+var btn = document.createElement("button"); 
+btn.innerHTML = "clicca"; 
+
+var body = document.getElementByTagNeame("body")[0]; 
+body.append(btn); 
+
+btn.addEventListener("click", function(){
+    document.getElementById("h1id").innerHTML = "ciao come stai?"; 
+}); 
