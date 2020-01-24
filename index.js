fe306e5466f76bf176852f5a68c4b175b002a2bb
De Biasi Riccardo <debiasi.riccardo@osdbsesto>
2020-01-24 10:11:34 +0100

    typo fixes on js & added button

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var btn = document.createElement("button"); 
 btn.innerHTML = "clicca"; 
 
-var body = document.getElementByTagNeame("body")[0]; 
+var body = document.getElementsByTagName("body")[0]; 
 body.append(btn); 
 
 btn.addEventListener("click", function(){
-    document.getElementById("h1id").innerHTML = "ciao come stai?"; 
+    document.getElementById("h1").innerHTML = "ciao come stai?"; 
 }); 
